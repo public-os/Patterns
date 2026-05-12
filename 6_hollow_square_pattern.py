9c7c30f2c29cd37ccf900e1d77682fe5a8485951
Tony <z999zon@gmail.com>
2026-05-12 11:24:54 +0530

    second commit

---
+++ 6_hollow_square_pattern.py
@@ -0,0 +1,12 @@
+n = 5
+for i in range(n):
+    if i==0 or i == n-1:
+        for j in range(n):
+            print("*",end='')
+    else:
+        for j in range(n):
+            if j==0 or j==n-1:
+                print("*",end='')
+            else:
+                print(" ",end='')
+    print()
