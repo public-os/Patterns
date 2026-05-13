13c89122b52b66ad397ad0d8c02399aad6b0cb1d
Tony <z999zon@gmail.com>
2026-05-13 23:57:22 +0530

    add

---
+++ 7.hollow_pyramid_pattern.py
@@ -0,0 +1,19 @@
+n = 5
+# for i in range(n):
+#     for j in range(n-i):
+#         print(" ",end="")
+#     for j in range(i+1):
+#         if j==0:
+#             print("*",end='')
+#         else:
+#             print(" ",end='')
+#     for j in range(i):
+#         if j==i-1:
+#             print("*",end='')
+#         else:
+#             print(" ",end='')
+#     if i==n-1:
+#         print()
+#         for j in range(n+n+1):
+#             print("*",end='')
+#     print()
